428a261932962be8b0619f957588ebf2af24ae1c
jmilthedude <30945719+jmilthedude@users.noreply.github.com>
2022-10-10 07:25:21 -0500

    missed condition

--- common/src/main/java/net/ninjadev/freelook/mixin/CameraMixin.java
+++ common/src/main/java/net/ninjadev/freelook/mixin/CameraMixin.java
@@ -1,6 +1,8 @@
 package net.ninjadev.freelook.mixin;
 
 import net.minecraft.client.Camera;
+import net.minecraft.client.CameraType;
+import net.minecraft.client.Minecraft;
 import net.ninjadev.freelook.event.CameraEvents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,7 +18,7 @@ public class CameraMixin {
 
     @Redirect(method = "setup", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Camera;setRotation(FF)V"))
     public void setRotation(Camera instance, float f, float g) {
-        //if (Minecraft.getInstance().options.getCameraType() != CameraType.FIRST_PERSON) return;
+        if (Minecraft.getInstance().options.getCameraType() == CameraType.THIRD_PERSON_FRONT) return;
         if (CameraEvents.shouldUpdate()) {
             CameraEvents.onCameraUpdate(instance);
         } else {
